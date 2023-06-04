7f69e707d34124602d779d7eb3d8621d5910cada
burenhuu <47760847+burenhuu@users.noreply.github.com>
2023-06-04 16:51:28 +0800

    Update index.jsx

--- src/i18n/index.jsx
+++ src/i18n/index.jsx
@@ -3,12 +3,14 @@ import arMessages from './messages/ar.json';
 import es419Messages from './messages/es_419.json';
 import frMessages from './messages/fr.json';
 import zhcnMessages from './messages/zh_CN.json';
+import mnMessages from './messages/mn.json';
 
 const messages = {
   ar: arMessages,
   'es-419': es419Messages,
   fr: frMessages,
   'zh-cn': zhcnMessages,
+  mn: mnMessages,
 };
 
 export default messages;
